29879ab9400233391455d75066a3f0a7462defa5
Your Name <you@example.com>
2020-03-17 12:42:44 +0530

    version 1.8.2

--- utils/Config.js
+++ utils/Config.js
@@ -4,7 +4,7 @@ require("reflect-metadata");
 exports.dbOptions = {
     name: "default",
     type: "postgres",
-    host: "locahost",
+    host: "localhost",
     port: 5432,
     username: "postgres",
     password: "Mpos1234",
