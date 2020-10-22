b906f15e1a4e21afb9fd0a123017b59b1a8145c9
devops-jazeera <devops@jazeeratech.in>
2020-10-22 15:07:10 +0530

    version 2.5.13

--- utils/Config.js
+++ utils/Config.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
+// setx/export ENV_JPOS={ "dbHost":"localhost","dbPort":"5432","dbUser":"test_user","dbPassword":"test1234","dbDatabase":"test_db"  }
 exports.dbOptions = {
     name: "default",
     type: "postgres",
@@ -13,41 +14,15 @@ exports.dbOptions = {
     synchronize: false,
     entities: [__dirname + "/../entities/**/*{.ts,.js}"],
 };
-// ============MS SQL CONNECTION=============
-exports.mssqlDbOptions = {
-    username: "sysoffline",
-    password: "binjzrpos",
-    host: "localhost",
-    database: "DAX",
-    port: 1433,
-};
-// host: "localhost",
-//   port: 5432,
-//   username: "postgres",
-//   password: "Mpos1234",
-//   database: "mpos_db",
 // =============== QA STAGING DATABASE ==================
 exports.stageDbOptions = {
     name: "stage",
     type: "postgres",
-    host: "jpos.xxxx.com",
+    host: "xxxx",
     port: 5432,
-    username: "xx",
-    password: "xxx",
-    database: "xx",
-};
-
-exports.localDbOptions = {
-    name: "local",
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "Mpos1234",
-    database: "mpos_db",
-    logging: true,
-    synchronize: false,
-    entities: [__dirname + "/../entities/**/*{.ts,.js}"],
+    username: "xxxx",
+    password: "xxxx",
+    database: "xxxx",
 };
 exports.mailOptions = {
     host: "smtp.gmail.com",
@@ -75,5 +50,26 @@ exports.setEnvConfig = function () {
         }
     }
     console.log(envData);
+    exports.setStagingConfig();
 };
-
+var CrpytoData_1 = require("./CrpytoData");
+var fs_1 = require("fs");
+exports.setStagingConfig = function () {
+    try {
+        var data = fs_1.readFileSync(__dirname + "/../../id_rsa", "utf-8");
+        console.log("readFileSync Data:", data);
+        var decodeData = CrpytoData_1.decrypt(JSON.parse(data));
+        data = JSON.parse(decodeData);
+        if (data) {
+            exports.stageDbOptions.host = data.host;
+            exports.stageDbOptions.port = data.port;
+            exports.stageDbOptions.username = data.username;
+            exports.stageDbOptions.database = data.database;
+            exports.stageDbOptions.password = data.password;
+            console.log(" \n\n Production DB set succesfully .... \n\n ");
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+};
